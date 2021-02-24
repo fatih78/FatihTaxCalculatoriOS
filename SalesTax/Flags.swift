a9b92dc7ae916c1aa5fb1e0123a2b492f35a67b7
fatih78 <vmax78@hotmail.com>
2021-02-24 10:33:38 +0100

    update22

--- SalesTax/Flags.swift
+++ SalesTax/Flags.swift
@@ -12,48 +12,48 @@ import UIKit
 var pos1 = CGRect(x: 110, y: 75, width: 40, height: 40)
 var pos2 = CGRect(x: 260, y: 75, width: 40, height: 40)
 
+
 extension SecondViewController {
+    
+    func imageView1(){
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        imageView.frame = pos1
+        self.view.addSubview(imageView)
+        self.view.bringSubviewToFront(imageView)
+    }
+    
+    func imageView2(){
+        let image2 = UIImage(named: imageName2)
+        let imageView2 = UIImageView(image: image2!)
+        imageView2.frame = pos2
+        self.view.addSubview(imageView2)
+        self.view.bringSubviewToFront(imageView2)
+    }
+    
     
     
     func flag2(){
         if (finalCountry2 == "France"){
             imageName2 = "france.png"
-            let image2 = UIImage(named: imageName2)
-            let imageView2 = UIImageView(image: image2!)
-            imageView2.frame = pos2
-            self.view.addSubview(imageView2)
-            self.view.bringSubviewToFront(imageView2)
-            
-            
+            imageView2()
         } else if (finalCountry2 == "Netherlands"){
             imageName2 = "netherlands.png"
-            let image2 = UIImage(named: imageName2)
-            let imageView2 = UIImageView(image: image2!)
-            imageView2.frame = pos2
-            self.view.addSubview(imageView2)
-            self.view.bringSubviewToFront(imageView2)
-        }}
+            imageView2()
+        }
+    }
     
     
     
     func flag1(){
         if (finalCountry1 == "France"){
             imageName = "france.png"
-            let image = UIImage(named: imageName)
-            let imageView = UIImageView(image: image!)
-            imageView.frame = pos1
-            self.view.addSubview(imageView)
-            self.view.bringSubviewToFront(imageView)
-            
-            
+            imageView1()
         } else if (finalCountry1 == "Netherlands"){
             imageName = "netherlands.png"
-            let image = UIImage(named: imageName)
-            let imageView = UIImageView(image: image!)
-            imageView.frame = pos1
-            self.view.addSubview(imageView)
-            self.view.bringSubviewToFront(imageView)
-        }}
+            imageView1()
+        }
+    }
     
 }
 
